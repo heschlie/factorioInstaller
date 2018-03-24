5381df13a9cb27a9d706dae25f094a14156791bb
Stephen Schlie <heschlie@gmail.com>
2018-03-24 15:15:35 -0700

    Initial commit
    Should function, let's try it out!

---
+++ main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"os"
+	"net/http"
+	"flag"
+	"fmt"
+	"io"
+
+	"factorioInstaller/models"
+
+	"github.com/mholt/archiver"
+	"encoding/json"
+	"io/ioutil"
+)
+
+const FACTORIO_URL = "https://www.factorio.com/get-download/stable/headless/linux64"
+const FACTORIO_DIR = "/opt/factorio"
+
+func main() {
+	token := flag.String("token", "", "Factorio account token")
+	serverName := flag.String("name", "My server", "Server name to display on server listings")
+	description := flag.String("description", "Created by golang script", "Server description")
+	saveFileUrl := flag.String("save", "", "The URL to download save file from")
+	password := flag.String("password", "", "Password for server, default is blank")
+	modsZipUrl := flag.String("modsUrl", "", "URL for a zip of mods to install")
+	flag.Parse()
+
+	// Create our settings file.
+	config := models.ServerSettings{
+		Name: *serverName,
+		Description: *description,
+		Tags: []string{},
+		MaxPlayers: 0,
+		Visibility: models.ServerVisibility{
+			Public: true,
+			Lan: true,
+		},
+		Token: *token,
+		GamePassword: *password,
+		RequireUserVerification: false,
+		MaxUpload: 0,
+		MinimumLatency: 0,
+		IgnorePlayerLimitForReturningPlayers: true,
+		AllowCommands: "admins-only",
+		AutosaveInterval: 10,
+		AutosaveSlots: 5,
+		AfkAutokickInterval: 0,
+		AutoPause: true,
+		OnlyAdminsCanPauseTheGame: true,
+		AutosaveOnlyOnServer: true,
+		Admins: []string{ "heschlie" },
+	}
+
+	// Download latest Factorio headless server into /opt/factorio.
+	os.MkdirAll(FACTORIO_DIR, 0755)
+	err := downloadFile(FACTORIO_DIR+"/factorio-headless.tar.gz", FACTORIO_URL)
+	if err != nil {
+		fmt.Errorf("failed to download server archive: %v", err)
+	}
+
+	// Unpack the tar.gz into /opt/factorio.
+	err = archiver.TarGz.Open(FACTORIO_DIR+"/factorio-headless.tar.gz", FACTORIO_DIR)
+	if err != nil {
+		fmt.Errorf("failed to extract archive: %v", err)
+	}
+
+	b, err := json.Marshal(config)
+	if err != nil {
+		fmt.Errorf("there was an error marshaling the config to json: %v", err)
+	}
+
+	err = ioutil.WriteFile(FACTORIO_DIR+"/config", b, 0644)
+	if err != nil {
+		fmt.Errorf("failed to save config: %v", err)
+	}
+
+	os.MkdirAll(FACTORIO_DIR+"/saves", 0755)
+	os.MkdirAll(FACTORIO_DIR+"/mods", 0755)
+
+	err = downloadFile(FACTORIO_DIR+"/saves/save.zip", *saveFileUrl)
+	if err != nil {
+		fmt.Errorf("failed to download save file: %v", err)
+	}
+
+	err = downloadFile(FACTORIO_DIR+"/mods/mods.zip", *modsZipUrl)
+	if err != nil {
+		fmt.Errorf("failed to download mods zip: %v", err)
+	}
+
+	err = archiver.Zip.Open(FACTORIO_DIR+"/mods/mods.zip", FACTORIO_DIR+"/mods")
+	if err != nil {
+		fmt.Errorf("failed to extract mods: %v", err)
+	}
+
+	os.Remove(FACTORIO_DIR+"/mods/mods.zip")
+
+	fmt.Println("Server ready to be launched! use the following command to launch:\n\n" +
+		"/opt/factorio/bin/x64/factorio --start-server /opt/factorio/saves/save.zip --server-settings /opt/factorio/config")
+}
+
+// downloadFile Will download a file from the specified URL.
+func downloadFile(filepath string, url string) (err error) {
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil  {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil  {
+		return err
+	}
+
+	return nil
+}
---
+++ models/serverSettings.go
@@ -0,0 +1,28 @@
+package models
+
+type ServerSettings struct {
+	Name                                 string
+	Description                          string
+	Tags                                 []string
+	MaxPlayers                           int `json:"max_players"`
+	Visibility                           ServerVisibility
+	Token                                string
+	GamePassword                         string `json:"game_password"`
+	RequireUserVerification              bool   `json:"require_user_verification"`
+	MaxUpload                            int    `json:"max_upload_in_kilobytes_per_second"`
+	MinimumLatency                       int    `json:"minimum_latency_in_ticks"`
+	IgnorePlayerLimitForReturningPlayers bool   `json:"ignore_player_limit_for_returning_players"`
+	AllowCommands                        string `json:"allow_commands"`
+	AutosaveInterval                     int    `json:"autosave_interval"`
+	AutosaveSlots                        int    `json:"autosave_slots"`
+	AfkAutokickInterval                  int    `json:"afk_autokick_interval"`
+	AutoPause                            bool   `json:"auto_pause"`
+	OnlyAdminsCanPauseTheGame            bool   `json:"only_admins_can_pause_the_game"`
+	AutosaveOnlyOnServer                 bool   `json:"autosave_only_on_server"`
+	Admins                               []string
+}
+
+type ServerVisibility struct {
+	Public bool
+	Lan    bool
+}
